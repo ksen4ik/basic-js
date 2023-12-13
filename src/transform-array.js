54dd015c9d8223d0e120f409d6f1dce0f0e6dfd3
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2023-12-14 01:00:51 +0300

    feat: update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -21,16 +21,19 @@ function transform(arr) {
                     let index00 = arr.indexOf('--discard-next');
                     arr.splice(index00, 2);
                 }
-            } else if(arr[i] == '--discard-prev') {
+            }
+            if(arr[i] == '--discard-prev') {
                 if(arr[i-1]) {
                     let index11 = arr.indexOf(arr[i-1]);
                     arr.splice(index11, 2);
                 }
-            } else if(arr[i] == '--double-next') {
+            }
+            if(arr[i] == '--double-next') {
                 if(arr[i+1]) {
                     arr[i] = arr[i+1];
                 }
-            } else if(arr[i] == '--double-prev') {
+            }
+            if(arr[i] == '--double-prev') {
                 if(arr[i-1]) {
                     arr[i] = arr[i-1];
                 }
